bd93e6593bf377b2ebd2b3fc7a516375aad44290
Donny Matchen <donnymatchen@gmail.com>
2019-09-07 16:36:43 +0000

    Basic Skeleton

---
+++ includes/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '') {
+    $config = [
+        'name' => '[FIXME: ADD SITE TITLE!]',
+        'pretty_uri' => false,
+        'site_url' => '[FIXME: ADD URL!]]',
+        'nav_menu' => [
+            '' => 'Home',
+            'search' => 'Search'
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v1.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'includes/config.php';
+require 'includes/functions.php';
+
+init();
