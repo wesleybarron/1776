5fc6fad3faaf5b85624283adee9a0eabfa036807
wesbarron <barron_wesley@columbusstate.edu>
2019-09-26 03:59:42 +0000

    adding some css for the search and search result pages.

--- template/search_result.php
+++ template/search_result.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?php page_title(); ?> | <?php site_name(); ?></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="/template/style.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
+
 
 <?php
+
+echo "<div id='search-result-div'><h1 style='text-align:center;'>Movie Results For: " . $search_key . "</h1></div>"
 
 $url = "http://www.omdbapi.com/?s=";
 
@@ -33,16 +26,10 @@ curl_close($handle);
 
 $output = "<ul>";
 foreach ($response['Search'] as $movie) {
-$output .= "<h3>".$movie['Title']."</h3>";
-$output .= "<li>".$movie['Year']."</li>";
-$output .= "<img src='" . $movie['Poster'] . "' width='250px' height='300px' alt='Comming Soon!'>";
+$output .= "<h3 style='text-align:center;'>".$movie['Title']."</h3>";
+$output .= "<li style='text-align:center;'>".$movie['Year']."</li>";
+$output .= "<img style='text-align:center;' src='" . $movie['Poster'] . "' width='250px' height='300px' alt='Comming Soon!'>";
 }
 $output .= "</ul>";
 echo $output;
 ?>
-
-</body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</html>
