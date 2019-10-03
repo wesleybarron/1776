240554aa041f36f66ceac5a9ee8695ddb3033f64
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-03 12:56:54 -0400

    attempt to fix the logo

--- template/template.php
+++ template/template.php
@@ -26,7 +26,7 @@
       <div class="mx-auto jumbotron jumbotron-fluid">
         <div class="container">
           <div class="media">
-            <img src="/assets/filmBUFF.png" class="align-self-center mr-3" width="150" height="150"alt="icon">
+            <img src="/assets/filmBUFF.png" class="align-self-center mr-3" alt="icon">
             <div class="media-body">
               <h1 class="mt-0"><?php site_name(); ?></h1>
             </div>
