37fffc6081b171b719d5b06ddc798aee79693f00
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-09 19:51:59 -0400

    fixed mistake in search_result.php

--- content/search.php
+++ content/search.php
@@ -12,6 +12,5 @@
 </div>
 <div class="row">
   <div id = "target_div">
-    
   </div>
 </div>
--- template/search_result.php
+++ template/search_result.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset ($_POST['search'])){
-    performsearch();
+    performSearch();
 } else{
     echo 'fail';
 };
