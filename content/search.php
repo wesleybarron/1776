227a197ff93869a4d956993ae417b2de4f3c2639
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-09 19:34:05 -0400

    added ajax script so that we can pull up search results within the search page

--- content/search.php
+++ content/search.php
@@ -1,10 +1,19 @@
-<form id="contact" action="template/search_result.php" target="_blank" method="get">
-<div class="input-group mb-3">
+<div class = "row">
+  <form id="contact" action="template/search_result.php" target="_blank" method="get">
+  <div class="input-group mb-3">
 
-  <input name="name" type="text" class="form-control" placeholder="Movie Title">
-  <div class="input-group-append"><br>
-    <button class="btn btn-success" type="submit">Go</button>
+    <input name="name" type="text" class="form-control" placeholder="Movie Title">
+    <div class="input-group-append"><br>
+      <button id= "dosearch" type= "button" class="btn btn-success" type="submit">Search</button>
 
+    </div>
+  </div>
+  </form>
+</div>
+<div class="row">
+  <div id = "target_div">
+    <?php
+      require 'template/search_result.php';
+    ?>
   </div>
 </div>
-</form>
