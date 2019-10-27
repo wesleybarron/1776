d81e64cb75e3723be132a6251bdeaedfd02577fb
wesbarron <barron_wesley@columbusstate.edu>
2019-10-27 23:28:21 +0000

    form back to get for now

--- content/search.php
+++ content/search.php
@@ -1,6 +1,6 @@
 <div class = "row">
   <div id = "search_form">
-    <form id="contact" action="/includes/search_result.php" method ="POST">
+    <form id="contact" action="/includes/search_result.php" method ="GET">
       <div class="input-group mb-3">
         <input name="name" type="text" class="form-control" placeholder="Movie Title">
         <div class="input-group-append"><br>
