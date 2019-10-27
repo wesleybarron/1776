58ca1081f2e87f8e045578aa8e0fe7acfe205b82
wesbarron <barron_wesley@columbusstate.edu>
2019-10-27 20:54:30 +0000

    changed php api fetch from get to post

--- includes/search_result.php
+++ includes/search_result.php
@@ -3,7 +3,7 @@
 function performSearch(){
     $url = "http://www.omdbapi.com/?s=";
 
-    $search_key = $_GET["name"];
+    $search_key = $_POST["name"];
     strtolower($search_key);
     $search_key = str_replace(" ", "+", $search_key);
     $api_key = "&apikey=d42aca4a";
