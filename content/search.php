d405fe8eca29cdaccc4d8a2ed5125b1c53d6dc66
wesbarron <barron_wesley@columbusstate.edu>
2019-10-29 01:22:31 +0000

    moved PHP to search page for now so movie results populate

--- content/search.php
+++ content/search.php
@@ -12,5 +12,48 @@
 </div>
 <div class="row">
   <div id = "target_div">
+  <?php
+
+$url = "http://www.omdbapi.com/?s=";
+
+    $search_key = $_GET["name"];
+    strtolower($search_key);
+    $search_key = str_replace(" ", "+", $search_key);
+    $api_key = "&apikey=d42aca4a";
+    $search_url = $url . $search_key . $api_key;
+
+    $handle = curl_init();
+    curl_setopt($handle, CURLOPT_URL, $search_url);
+    curl_setopt_array($handle,
+    array(
+        CURLOPT_URL => $search_url,
+        CURLOPT_RETURNTRANSFER => true
+    )
+    );
+    $output = curl_exec($handle);
+    $response = json_decode($output, true);
+    curl_close($handle);
+
+    $upper_search_key = strtoupper($search_key);
+    echo "<h1 style='text-align:center;'>Movie Results For: " . $upper_search_key . "</h1>";
+
+    $output = "<ul>";
+    foreach ($response['Search'] as $movie) {
+    $output .= "<h3 style='text-align:center;'>".$movie['Title']."</h3>";
+    $output .= "<li style='text-align:center;list-style-type:none;'>".$movie['Year']."</li>";
+    $output .= "<div style='margin:auto;text-align:center;'>";
+
+    if ($movie['Poster'] == "N/A") {
+        $output .= "<img src='https://media2.giphy.com/media/d2ZhZTK55EA2yvTy/200.webp?cid=790b76115b079f19e6b19b7cc0a624173b4015791c3264c1&rid=200.webp' width='250px' height='300px'>";
+        }else {
+            $output .= "<img src='" . $movie['Poster'] . "' width='250px' height='300px'>";
+        }
+
+    $output .= "</div>";
+    }
+    $output .= "</ul>";
+    echo $output;
+
+  ?>
   </div>
 </div>
