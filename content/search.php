727b39c2e6ac32e4ba9a8936721cae790f35bd9e
wesbarron <barron_wesley@columbusstate.edu>
2019-10-29 02:54:58 +0000

    messing with some bootstrap

--- content/search.php
+++ content/search.php
@@ -46,8 +46,8 @@ $url = "http://www.omdbapi.com/?s=";
     $output = "<div>";
 
     foreach ($response['Search'] as $movie) {
-    $output = "<div class='col-md-3'>";
-    $output = "<div class='well text-center'>";
+    $output .= "<div class='col-md-3'>";
+    $output .= "<div class='well text-center'>";
     $output .= "<h3 style='text-align:center;'>".$movie['Title']."</h3>";
     $output .= "<li style='text-align:center;list-style-type:none;'>".$movie['Year']."</li>";
     $output .= "<div style='margin:auto;text-align:center;'>";
