d43135c1643d4ab9f61ba6f126fedc7034b470a9
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-31 16:25:04 -0400

    trying to extend jumbotron

--- content/home.php
+++ content/home.php
@@ -3,7 +3,7 @@
           <div class="media">
             <img src="/assets/filmbuff.png" class="align-self-center mr-3" width="150" height="150"alt="icon">
             <div class="media-body">
-              <h1 class="mt-0"><?php site_name(); ?></h1>
+              <h1 class="display-4 mt-0"><?php site_name(); ?></h1>
             </div>
           </div>
         </div>
