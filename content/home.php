c55eb5150d4b7594084997fcb989a3cfd3c4e23e
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-31 12:36:00 -0400

    moved jumbotron to just home page

--- content/home.php
+++ content/home.php
@@ -1,1 +1,10 @@
-Welcome to FilmBuff!
+<div class="mx-auto jumbotron jumbotron-fluid">
+        <div class="container">
+          <div class="media">
+            <img src="/assets/filmbuff.png" class="align-self-center mr-3" width="150" height="150"alt="icon">
+            <div class="media-body">
+              <h1 class="mt-0"><?php site_name(); ?></h1>
+            </div>
+          </div>
+        </div>
+      </div>
