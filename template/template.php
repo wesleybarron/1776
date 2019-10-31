87efff64469bc3688a95ab878dbedd6622409fef
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-31 12:40:11 -0400

    revert changes something broke

--- template/template.php
+++ template/template.php
@@ -23,6 +23,16 @@
 
 
 <div>
+      <div class="mx-auto jumbotron jumbotron-fluid">
+        <div class="container">
+          <div class="media">
+            <img src="/assets/filmbuff.png" class="align-self-center mr-3" width="150" height="150"alt="icon">
+            <div class="media-body">
+              <h1 class="mt-0"><?php site_name(); ?></h1>
+            </div>
+          </div>
+        </div>
+      </div>
   <div class ="row">
     <div class="mx-auto">
       <article>
