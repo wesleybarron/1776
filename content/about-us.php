74a51598a00267eb40dbd8a4cf1b8762591e804f
wesbarron <barron_wesley@columbusstate.edu>
2019-11-03 22:56:02 +0000

    working on about-us page

--- content/about-us.php
+++ content/about-us.php
@@ -1,2 +1,3 @@
 <p>Filmbuff is a site that lets you search for information on movies.  Our site is powered by the OMDB API.</p>
-<p>Special thanks to all of the bellow for assets and other contributions.</p>
+<p>Special thanks to all of the below for assets and other contributions.</p>
+<img id="Danny" src="/assets/danny.jpg">
