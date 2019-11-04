f8105086fd0ea870e61fe9752f37b4254fc73f54
wesbarron <barron_wesley@columbusstate.edu>
2019-11-04 05:19:57 +0000

    hide jumbotron on search page

--- content/search.php
+++ content/search.php
@@ -11,34 +11,7 @@
   </div>
 </div>
 
-<!-- The Modal -->
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
 
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 id="movie-title" class="modal-title">Title</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-
-      <!-- Modal body -->
-      <div class="modal-body">
-        <h6>Rating:</h6 &nbsp;><p id="movie-rating"></p>
-        <p id="movie-rating"></p>
-        <p id="movie-rating"></p>
-        <p id="movie-rating"></p>
-        <p id="movie-rating"></p>
-      </div>
-
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-      </div>
-
-    </div>
-  </div>
-</div>
 
 
 
@@ -98,6 +71,34 @@ $url = "http://www.omdbapi.com/?s=";
         $output .= "<br>";
         $output .= '<button id="my-button" type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Movie Details</button>';
         $output .= "<br><br>";
+        //<!-- The Modal -->
+        $output .= '<div class="modal" id="myModal">
+                    <div class="modal-dialog">
+                    <div class="modal-content">
+
+      <!-- Modal Header -->
+                    <div class="modal-header">
+                    <h4 id="movie-title" class="modal-title">Title</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+      <!-- Modal body -->
+                    <div class="modal-body">
+                    <h6>Rating:</h6 &nbsp;><p id="movie-rating"></p>
+                    <p id="movie-rating"></p>
+                    <p id="movie-rating"></p>
+                    <p id="movie-rating"></p>
+                    <p id="movie-rating"></p>
+                    </div>
+
+      <!-- Modal footer -->
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>';
         }else {
             $output .= "<img class='movie-poster' src='" . $movie['Poster'] . "'>";
             $output .= "<br>";
@@ -106,6 +107,34 @@ $url = "http://www.omdbapi.com/?s=";
             $output .= "<p id='movie-id'>" .$movie['imdbID']."</p>";
             $output .= '<button id="my-button" type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Movie Details</button>';
             $output .= "<br><br>";
+             //<!-- The Modal -->
+            $output .= '<div class="modal" id="myModal">
+                    <div class="modal-dialog">
+                    <div class="modal-content">
+
+      <!-- Modal Header -->
+                    <div class="modal-header">
+                    <h4 id="movie-title" class="modal-title">Title</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+      <!-- Modal body -->
+                    <div class="modal-body">
+                    <h6>Rating:</h6 &nbsp;><p id="movie-rating"></p>
+                    <p id="movie-rating"></p>
+                    <p id="movie-rating"></p>
+                    <p id="movie-rating"></p>
+                    <p id="movie-rating"></p>
+                    </div>
+
+      <!-- Modal footer -->
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>';
         }
 
     $output .= "</div>";
