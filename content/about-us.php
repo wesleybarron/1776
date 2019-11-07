a865dc7b1e8a373394a7caae246a8d3841c539ab
wesbarron <54125356+wesbarron@users.noreply.github.com>
2019-11-07 17:22:16 -0500

    Update about-us.php

--- content/about-us.php
+++ content/about-us.php
@@ -5,3 +5,32 @@
     </div>
 <p>Filmbuff is a site that lets you search for information on movies.  Our site is powered by the OMDB API.</p>
 <p>Special thanks to all of the bellow for assets and other contributions.</p>
+<h2>1776 Is:</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="well text-center">
+            <img id="Donny" src="/assets/donny.jpg" width="160px" height="220px">
+            </div>
+            <h5>Donny Matchen</h5>
+        </div>
+        <div class="col-md-3">
+            <div class="well text-center">
+            <img id="Anthony" src="/assets/anthony.jpg" width="160px" height="220px">
+            </div>
+            <h5>Anthony Obando</h5>
+        </div>
+        <div class="col-md-3">
+            <div class="well text-center">
+            <img id="Josh" src="/assets/josh.jpg" width="160px" height="220px">
+            </div>
+            <h5>Josh Lopez</h5>
+        </div>
+        <div class="col-md-3">
+            <div class="well text-center">
+            <img id="Wes" src="/assets/wes.jpg" width="160px" height="220px">
+            </div>
+            <h5>Wes Barron</h5>
+        </div>
+    </div>
+</div>
